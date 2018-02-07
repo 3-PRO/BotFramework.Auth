6e449d23c2338fc7b613a283251cb6133001d8f3
Ivan Rezek <irezek@3pro.eu>
2018-02-07 11:12:34 +0100

    Bugfix for TRIPRO.BotFramework.Auth.AspNetCore.resources was incorrectly embedded or linked into assembly.

--- TRIPRO.BotFramework.Auth.AspNetCore/AuthText.Designer.cs
+++ TRIPRO.BotFramework.Auth.AspNetCore/AuthText.Designer.cs
@@ -39,7 +39,7 @@ namespace TRIPRO.BotFramework.Auth.AspNetCore {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("BotAuth.AspNetCore.AuthText", typeof(AuthText).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("TRIPRO.BotFramework.Auth.AspNetCore.AuthText", typeof(AuthText).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
